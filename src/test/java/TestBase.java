af567954f2db19ef9807de4177850353506d14bc
Isaieva Mariia <maria.m.isaieva@icloud.com>
2022-11-28 16:02:14 +0100

    TestGrowLab TestBase TestsAllUserAuthorization

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -10,77 +10,93 @@ public class TestBase {
 
 
     @BeforeTest
-    public void prepare(){
+    public void prepare() {
         wd = new FirefoxDriver();
         wd.get("https://derrick686.softr.app/");
         wd.manage().window().maximize();
-        click(By.xpath("//*[@id=\"home-header2\"]/div/div[1]/ul/li[2]/a"));
 
 
     }
+
     @AfterTest
-    public void afterTest(){
+    public void afterTest() {
         wd.quit();
     }
 
 
-
-    public void click(By locator){
+    public void click(By locator) {
         wd.findElement(locator).click();
 
     }
 
 
-    public void clear(By locator){
+    public void clear(By locator) {
         wd.findElement(locator).clear();
     }
 
 
-    public void type(By locator, String text){
+    public void type(By locator, String text) {
         wd.findElement(locator).sendKeys(text);
     }
 
+    public void login() {
+        click(By.xpath("//*[@id=\"home-header2\"]/div/div[1]/ul/li[2]/a"));
+    }
 
-    public void email(String email){
+
+    public void email(String email) {
         click(By.xpath("//*[@id=\"sw-form-capture-email-input\"]"));
         clear(By.xpath("//*[@id=\"sw-form-capture-email-input\"]"));
         type(By.xpath("//*[@id=\"sw-form-capture-email-input\"]"), email);
     }
 
-    public void password(){
+    public void password() {
         click(By.id("sw-form-password-input"));
         clear(By.id("sw-form-password-input"));
         type(By.id("sw-form-password-input"), "123456");
     }
 
-    public void signIn(){
+    public void signIn() {
         click(By.id("sw-sign-in-submit-btn"));
 
     }
 
-    public void logOut(){
+    public void pause(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void logOut() {
         click(By.id("navbarDropdown"));
-        acceptText("           Sign Out          ");
-        click(By.xpath("/html/body/div[1]/nav/div/div[1]/ul/li[5]/div/a/span/span"));
-
-
+        acceptText("Sign Out");
+        click(By.cssSelector("div.show > a:nth-child(1)"));
 
     }
 
-
-
-
-    public void acceptText(String acceptText){
+    public void acceptText(String acceptText) {
         String source = wd.getPageSource();
-        //String acceptText="Welcome to your Client Portal";
         Assert.assertTrue(source.contains(acceptText));
     }
-
-    public void alertText(){
+    public void acceptNoText(String acceptNoText) {
         String source = wd.getPageSource();
-        String alertText="Invalid email or password";
+        Assert.assertFalse(source.contains(acceptNoText));
+    }
+
+    public void alertText(String alertText) {
+        String source = wd.getPageSource();
         Assert.assertTrue(source.contains(alertText));
     }
+
+//    public boolean isClientsComponentPresent(){
+//        try{
+//            wd.findElement(By.xpath("/html/body/div[1]/nav/div/div[1]/ul/li[2]/a");
+//        } catch (NoSuchElementExeptoin ex){
+//        }
+//        return false;
+//    }
 
 
 }
--- src/test/java/TestGrowLab23.java
+++ src/test/java/TestGrowLab23.java
@@ -1,117 +1,59 @@
 import org.testng.annotations.Test;
 
-    public class TestGrowLab23 extends TestBase {
+public class TestGrowLab23 extends TestBase {
 
 
+    @Test
+    public void negativTest1() throws InterruptedException {
 
-        @Test
-        public void negativTest1() throws InterruptedException {
+        pause(5000);
 
-            Thread.sleep(5000);
+        email("billyeexample.com");
+        pause(2000);
 
-            email("billyeexample.com");
-            Thread.sleep(2000);
+        password();
+        pause(2000);
 
-            password();
-            Thread.sleep(2000);
+        signIn();
+        pause(2000);
 
-            signIn();
-            Thread.sleep(2000);
+        alertText("Invalid email or password");
 
-           alertText();
+    }
 
-        }
+    @Test
+    public void negativTest2() throws InterruptedException {
 
-        @Test
-        public void negativTest2() throws InterruptedException {
+        pause(5000);
 
-            Thread.sleep(5000);
+        password();
+        pause(2000);
 
-            password();
-            Thread.sleep(2000);
+        signIn();
+        pause(2000);
 
-            signIn();
-            Thread.sleep(2000);
+        alertText("Invalid email or password");
 
-            alertText();
+    }
 
-        }
-        @Test
-        public void negativTest3() throws InterruptedException {
+    @Test
+    public void negativTest3() throws InterruptedException {
 
-            Thread.sleep(5000);
+        pause(5000);
 
-            email("lucie@example.com");
-            Thread.sleep(2000);
+        email("lucieexample.com");
+        pause(5000);
 
-            password();
-            Thread.sleep(2000);
+        password();
+        pause(5000);
 
-            signIn();
-            Thread.sleep(2000);
+        signIn();
+        pause(5000);
 
-            alertText();
+        alertText("Invalid email or password");
 
 
-        }
-
-        @Test
-        public void positiveManagerTest() throws InterruptedException {
-
-            Thread.sleep(5000);
-
-            email("billye@example.com");
-            Thread.sleep(2000);
-
-            password();
-            Thread.sleep(2000);
-
-            signIn();
-            Thread.sleep(7000);
-
-            acceptText("Welcome to your Client Portal");
-
-        }
-
-        @Test
-        public void positiveClientTest() throws InterruptedException {
-
-            Thread.sleep(5000);
-
-            email("lucie@example.com");
-            Thread.sleep(2000);
-
-            password();
-            Thread.sleep(2000);
-
-            signIn();
-            Thread.sleep(7000);
-
-            acceptText("Welcome to your Client Portal");
-
-        }
-
-        @Test
-        public void positiveConsultantTest() throws InterruptedException {
-
-            Thread.sleep(5000);
-
-            email("lucie@example.com");
-            Thread.sleep(2000);
-
-            password();
-            Thread.sleep(2000);
-
-            signIn();
-            Thread.sleep(7000);
-
-            acceptText("Welcome to your Client Portal");
-
-            logOut();
-            //Boolean.FALSE
-
-        }
-
+    }
 
 
 }
